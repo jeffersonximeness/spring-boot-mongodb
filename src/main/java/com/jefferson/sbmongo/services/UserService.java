394a7a2a35e7c223261bea70db1e15279d9037ac
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-09-26 18:49:15 -0300

    obtendo usuario por id

--- src/main/java/com/jefferson/sbmongo/services/UserService.java
+++ src/main/java/com/jefferson/sbmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jefferson.sbmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jefferson.sbmongo.domain.User;
 import com.jefferson.sbmongo.repository.UserRepository;
+import com.jefferson.sbmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 }
